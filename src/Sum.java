aa936501b1b3a80b9768084f3f470b0c47a69b9a
Dileema <dileematl@gmail.com>
2017-03-02 18:36:14 +0530

    Add three java files

--- src/Sum.java
+++ src/Sum.java
@@ -7,8 +7,6 @@ public class Sum {
         for(int i=1;i<1000;i++){
             if(i%3==0 || i%5==0) {
                 sum = sum + i;
-            }else{
-                sum=sum;
             }
         }
         System.out.println("Total sum=" + sum);
